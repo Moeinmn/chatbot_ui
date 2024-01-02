24ff0f7925c8d2748cc8f2199e23e25a83a5aa6e
moein moeinnia <moein@pro.ai>
2024-01-02 16:24:11 +0330

    first

---
+++ src/app/Box.jsx
@@ -0,0 +1,81 @@
+'use client';
+
+import clsx from "clsx";
+import Image from "next/image";
+import { useState } from "react";
+import Avatar from "./Avatar";
+//import { format } from "date-fns";
+// import { useSession } from "next-auth/react";
+// import { FullMessageType } from "@/app/types";
+
+//import Avatar from "@/app/components/Avatar";
+//import ImageModal from "./ImageModal";
+
+// interface MessageBoxProps {
+//   data: FullMessageType;
+//   isLast?: boolean;
+// }
+
+const MessageBox = ({ 
+  data, 
+  isLast
+}) => {
+//   const session = useSession();
+  const [imageModalOpen, setImageModalOpen] = useState(false);
+
+
+//  const isOwn = session.data?.user?.email === data?.sender?.email
+const isOwn = Math.round(Math.random());
+
+//   const seenList = (data.seen || [])
+
+//   .filter((user) => user.email !== data?.sender?.email)
+//     .map((user) => user.name)
+//     .join(', ');
+  const seenList = []
+
+  const container = clsx('flex gap-3 p-4', isOwn && 'justify-end');
+  const avatar = clsx(isOwn && 'order-2');
+  const body = clsx('flex flex-col gap-2', isOwn && 'items-end');
+  const message = clsx(
+    'text-sm w-fit overflow-hidden', 
+    isOwn ? 'bg-sky-500 text-white' : 'bg-gray-100', 
+    data.image ? 'rounded-md p-0' : 'rounded-full py-2 px-3'
+  );
+
+  return ( 
+    <div className={container}>
+      <div className={avatar}>
+        <Avatar user={data?.sender} />
+      </div>
+      <div className={body}>
+        <div className="flex items-center gap-1">
+          <div className="text-sm text-gray-500">
+            {"Moein"}
+            {/* {data?.sender?.name || "Moein"} */}
+          </div>
+          {/* <div className="text-xs text-gray-400">
+            {format(new Date(data.createdAt), 'p')}
+          </div> */}
+        </div>
+        <div className={message}>
+          {/* <ImageModal src={data.image} isOpen={imageModalOpen} onClose={() => setImageModalOpen(false)} /> */}
+            <div>{data.body}</div>
+        </div>
+        {/* {isLast && isOwn && seenList?.length > 0 && (
+          <div 
+            className="
+            text-xs 
+            font-light 
+            text-gray-500
+            "
+          >
+            {`Seen by `}
+          </div>
+        )} */}
+      </div>
+    </div>
+   );
+}
+ 
+export default MessageBox;
